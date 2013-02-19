cfd0cd6ff8e626c656e26cbac736097982b48927
Quentin Calvez <quentin.calvez@quentez.com>
2013-02-19 11:30:04 +0100

    Unified commenting style for the BarGraph driver.

--- drivers/bargraph/bargraph.c
+++ drivers/bargraph/bargraph.c
@@ -13,35 +13,20 @@
 #define bitclr(var,bitno) ((var) &= ~(1 << (bitno)))
 
 
-
-#define		DISP_ON			0x0c	        //LCD control constants
-#define		DISP_OFF		0x08	        //
-#define		CLR_DISP		0x01    	//
-#define		CUR_HOME		0x02	        //
-#define		ENTRY_INC		0x06            //
-#define		DD_RAM_ADDR		0x80	        //
-#define		DD_RAM_ADDR2		0xc0	        //
-#define		DD_RAM_ADDR3		0x28	        //
-#define		CG_RAM_ADDR		0x40	        //
+// LCD Control Constants
+#define		DISP_ON				0x0c
+#define		DISP_OFF			0x08
+#define		CLR_DISP			0x01
+#define		CUR_HOME			0x02
+#define		ENTRY_INC			0x06
+#define		DD_RAM_ADDR			0x80
+#define		DD_RAM_ADDR2		0xc0
+#define		DD_RAM_ADDR3		0x28
+#define		CG_RAM_ADDR			0x40
 
 // Necessaire a cause d'un bug d'insight
 int TMP1,TMP2;
 unsigned int val=1;
-
-
-/*static void delay(unsigned int minor, unsigned int major)
-{
-  int i,j;
-  for(j=0; j < major; j++)
-    {
-      for (i = 0; i<minor; i++) 
-	   {
-	     nop();
-	     nop();
-	   }
-    }
-}
-*/
 
 void Bargraph_Print(unsigned char segment, unsigned char state)
 {
@@ -60,57 +45,57 @@ void Bargraph_Print(unsigned char segment, unsigned char state)
 	switch (segment)
 	{
 		case (0): 
-		{	S0_OFF;		//line selection 1/8 (msb) S2 S1 S0 (lsb)
+		{	S0_OFF;		// Line selection 1/8 (msb) S2 S1 S0 (lsb)
 			S1_OFF;
 			S2_OFF;
 			break;
 		}
 		case (1): 
-		{	S0_ON;		//line selection 1/8 (msb) S2 S1 S0 (lsb)
+		{	S0_ON;		// Line selection 1/8 (msb) S2 S1 S0 (lsb)
 			S1_OFF;
 			S2_OFF;
 			break;
 		}
 		case (2): 
-		{	S0_OFF;		//line selection 1/8 (msb) S2 S1 S0 (lsb)
+		{	S0_OFF;		// Line selection 1/8 (msb) S2 S1 S0 (lsb)
 			S1_ON;
 			S2_OFF;
 			break;
 		}
 		case (3): 
-		{	S0_ON;		//line selection 1/8 (msb) S2 S1 S0 (lsb)
+		{	S0_ON;		// Line selection 1/8 (msb) S2 S1 S0 (lsb)
 			S1_ON;
 			S2_OFF;
 			break;
 		}
 		case (4): 
-		{	S0_OFF;		//line selection 1/8 (msb) S2 S1 S0 (lsb)
+		{	S0_OFF;		// Line selection 1/8 (msb) S2 S1 S0 (lsb)
 			S1_OFF;
 			S2_ON;
 			break;
 		}
 		case (5): 
-		{	S0_ON;		//line selection 1/8 (msb) S2 S1 S0 (lsb)
+		{	S0_ON;		// Line selection 1/8 (msb) S2 S1 S0 (lsb)
 			S1_OFF;
 			S2_ON;
 			break;
 		}
 		case (6): 
-		{	S0_OFF;		//line selection 1/8 (msb) S2 S1 S0 (lsb)
+		{	S0_OFF;		// Line selection 1/8 (msb) S2 S1 S0 (lsb)
 			S1_ON;
 			S2_ON;
 			break;
 		}
 		case (7): 
-		{	S0_ON;		//line selection 1/8 (msb) S2 S1 S0 (lsb)
+		{	S0_ON;		// Line selection 1/8 (msb) S2 S1 S0 (lsb)
 			S1_ON;
 			S2_ON;
 			break;
-		}
-	
+		}	
 		default: break;
 	}
-	SEL_OFF;	//LATCH
+	
+	SEL_OFF;	// LATCH
 	SEL_ON;	
 }
 
@@ -148,5 +133,3 @@ void Bargraph_Fall(void)
 		Bargraph_Print(i,LED_OFF);
 	}
 }
-
-
