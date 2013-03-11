55d9ca5f9ce93a672f8e3b5f8075408058a45862
fduranton <f.m.duranton@gmail.com>
2013-03-11 19:33:01 +0100

    Move hal_usb, rx and tx tasks

--- app/source/tasks/rx_task.h
+++ app/source/tasks/rx_task.h
@@ -3,17 +3,42 @@
 
 #include <includes.h>
 
-/*******************************************************************************
-* Variables.
-*******************************************************************************/
+/*-------------------------------------------------------------
+ *                  USB global definitions
+ * ------------------------------------------------------------*/
+#include "hal_MSP-EXP430F5438.h"
 
+#define USB_PORT_OUT      P5OUT
+#define USB_PORT_SEL      P5SEL
+#define USB_PORT_DIR      P5DIR
+#define USB_PORT_REN      P5REN
+#define USB_PIN_TXD       BIT6
+#define USB_PIN_RXD       BIT7
+
+#define BUFFER_SIZE				255
+
+/*-------------------------------------------------------------
+ *                  Function Prototypes
+ * ------------------------------------------------------------*/
+extern void halUsbInit(void);
+extern void halUsbShutDown(void);
+extern void halUsbSendChar(char character);
+
+
+extern void halUsbSendString(char string[], unsigned char length);
+extern void halUsbReceiveString(char string[], unsigned char *length);
+
+
+/*-------------------------------------------------------------
+ *                  Variables
+ * ------------------------------------------------------------*/
 // Tasks.
 static OS_STK RxTaskStack[RX_TASK_STACK_SIZE];
 
-/*******************************************************************************
-* Function prototypes.
-*******************************************************************************/
 
+/*-------------------------------------------------------------
+ *                  Function Prototypes
+ * ------------------------------------------------------------*/
 // Public.
 void RxTask(void *args);
 
