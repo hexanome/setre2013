ad92e205d345f5e121c9625d786a94c7a42b5c91
Kevin Ferrier <kevin@Kekeland.(none)>
2013-03-12 09:56:22 +0100

    Dev Record Task

--- app/source/tasks/record_task.h
+++ app/source/tasks/record_task.h
@@ -14,6 +14,8 @@
 #define REC_START                       0
 #define REC_SEND                        2
 #define QUEUE_SYNCDMA_LENGTH            1
+#define NUMBER_OF_SEGMENTS              12
+#define SIZE_OF_SEGMENTS                0x4000
 
 /*******************************************************************************
 * Variables.
@@ -38,6 +40,7 @@ static void *qSyncDMAData[QUEUE_SYNCDMA_LENGTH];
 void RecordTask(void *args);
 
 // Private.
+static INT32U getSegmentAddress(INT8U index);
 static void setupRecord(void);
 static void stopRecord(void);
 static void record(void);
